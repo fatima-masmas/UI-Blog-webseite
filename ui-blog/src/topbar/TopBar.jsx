02339da0e0ce702185c9a6ebe1674320de503540
fatima-masmas <87-f@gmx.de>
2023-02-07 21:56:44 +0100

    Topbar bereich

--- ui-blog/src/topbar/TopBar.jsx
+++ ui-blog/src/topbar/TopBar.jsx
@@ -4,7 +4,25 @@ import "./topbar.css";
 export default function Topbar() {
     return (
         <div className="top">
-            TopBar
+            <div className="topLeft">L 
+             <i class="fa-brands fa-facebook"></i>
+             <i class="fa-brands fa-twitter"></i>
+             <i class="fa-brands fa-pinterest"></i>
+             <i class="fa-brands fa-square-instagram"></i></div>
+            <div className="topCenter">
+                <ul className="topList">
+                    <li className="topListItem">HOME</li>
+                    <li className="topListItem">ABOUT</li>
+                    <li className="topListItem">CONTACT</li>
+                    <li className="topListItem">WRITE</li>
+                    <li className="topListItem">LOGIN</li>
+                    
+                </ul>
+            </div>
+            <div className="topRight">
+                <img className="topImg" src="" alt="" />
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
 
         </div>
     )
