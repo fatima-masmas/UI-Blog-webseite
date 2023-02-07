32e8fdd2f0868a6c12de08d8593440a3f361257a
fatima-masmas <87-f@gmx.de>
2023-02-08 00:05:21 +0100

    header ist fertig

--- ui-blog/src/header/Header.jsx
+++ ui-blog/src/header/Header.jsx
@@ -8,7 +8,14 @@ export default function Header() {
                 <span className="headerTitleLg">Blog</span>
 
             </div>
-            <img className="headerImg" src="../topbar/img/img_girl.jpg" alt="" />
+            <img className="headerImg" src="https://res.cloudinary.com/stealthman22/image/upload/v1586308023/new-portfolio/hero/two-cargo-ships-sailing-near-city-2144905.jpg" alt="" />
         </dev>
     )
 }
+
+// 'https://res.cloudinary.com/stealthman22/image/upload/v1586308024/new-portfolio/hero/time-lapse-photography-of-waterfalls-during-sunset-210186.jpg'
+// https://cdn.pixabay.com/photo/2021/09/02/16/48/cat-6593947_960_720.jpg
+// https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350  => S.Cat
+// https://cdn.pixabay.com/photo/2021/09/02/16/48/cat-6593947_960_720.jpg G.Cat
+
+// https://picsum.photos/200 verschiedene Fotos 
