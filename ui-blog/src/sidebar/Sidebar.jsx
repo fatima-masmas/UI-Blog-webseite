58ecb6abd8648446df2916ada9f05c3a12fbd9a1
fatima-masmas <87-f@gmx.de>
2023-02-08 00:52:23 +0100

    sidebar seite erstellen

--- ui-blog/src/sidebar/Sidebar.jsx
+++ ui-blog/src/sidebar/Sidebar.jsx
@@ -3,7 +3,35 @@ import "./sidebar.css";
 export default function Sidebar() {
     return (
         <div className="sidebar">
-            sidebar
+            <div className="sidebarItem">
+                <span className="sidebarTitle">ABOUT ME</span>
+                <img src="https://picsum.photos/200" alt="" />
+                <p>
+                In React applications, Images are served from different folder locations.
+                public folder src folder
+                </p>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">CATEGORIES</span>
+                <ul className= "sidebarList">
+                    <li className="sidebarListItem">Life</li>
+                    <li className="sidebarListItem">Music</li>
+                    <li className="sidebarListItem">Style</li>
+                    <li className="sidebarListItem">Sport</li>
+                    <li className="sidebarListItem">Tech</li>
+                    <li className="sidebarListItem">Cinema</li>
+                    <li className="sidebarListItem">Theater</li>
+                </ul>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">FOLLOW US</span>
+                <div className="sidebarSocial">
+                <i className="sidebarIcon fa-brands fa-facebook"></i>
+                <i className="sidebarIcon fa-brands fa-twitter"></i>
+                <i className="sidebarIcon fa-brands fa-pinterest"></i>
+                <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+                </div>
+            </div>
         </div>
     )
 }
