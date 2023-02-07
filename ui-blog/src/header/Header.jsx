60e0730dce19ae08e6decd83fdb8bdcb6111d267
fatima-masmas <87-f@gmx.de>
2023-02-07 22:46:21 +0100

    header und homepages erstellen

---
+++ ui-blog/src/header/Header.jsx
@@ -0,0 +1,14 @@
+import "./header.css";
+
+export default function Header() {
+    return (
+        <dev className="header">
+            <div className="headerTitle">
+                <span className="headerTitleSm">React & Node</span>
+                <span className="headerTitleLg">Blog</span>
+
+            </div>
+            <img className="headerImd" src="./" alt="" />
+        </dev>
+    )
+}
