abe3d692cc62ecdddc674001e5635902d7472396
fatima-masmas <87-f@gmx.de>
2023-02-08 14:24:27 +0100

    die formen sind schon bereit bei login

--- ui-blog/src/pages/login/Login.jsx
+++ ui-blog/src/pages/login/Login.jsx
@@ -3,7 +3,14 @@ import "./login.css"
 export default function Login() {
   return (
     <div className="login">
-        Login
+        <from className="loginForm">
+            <label >E-mail</label>
+            <input type="text" placeholder="Enter your Email..." />
+            <label >Passwort</label>
+            <input type="passwort" placeholder="Enter your Passwort..." />
+            <button className="loginButton">Log In</button>
+            <button className="loginRegisterButton">Register</button>
+        </from>
       
     </div>
   )
