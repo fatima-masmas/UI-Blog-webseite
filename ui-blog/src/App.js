9b90bf188f843732bf1579b516d9cfd810db8e0c
fatima-masmas <87-f@gmx.de>
2023-02-08 09:06:21 +0100

    alle datein in einen Component datei zusammen

--- ui-blog/src/App.js
+++ ui-blog/src/App.js
@@ -1,5 +1,5 @@
 import Home from "./pages/home/Home";
-import Topbar from "./topbar/TopBar";
+import Topbar from "./components/topbar/TopBar";
 
 
 
--- ui-blog/src/pages/home/Home.jsx
+++ ui-blog/src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-no-undef */
-import Header from "../../header/Header";
-import Sidebar from "../../sidebar/Sidebar";
-import Posts from "../../posts/Posts";
+import Header from "../../components/header/Header";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Posts from "../../components/posts/Posts";
 import "./home.css";
 
 export default function Home() {
