e4d4012b27b5b5d738d7886df89d853dff5cc2a5
fatima-masmas <87-f@gmx.de>
2023-02-08 15:42:44 +0100

    login page ist fertig

--- ui-blog/src/pages/login/Login.jsx
+++ ui-blog/src/pages/login/Login.jsx
@@ -3,11 +3,12 @@ import "./login.css"
 export default function Login() {
   return (
     <div className="login">
+        <span className="loginTitle">Log In</span>
         <from className="loginForm">
             <label >E-mail</label>
-            <input type="text" placeholder="Enter your Email..." />
+            <input type="text" className="loginInput" placeholder="Enter your Email..." />
             <label >Passwort</label>
-            <input type="passwort" placeholder="Enter your Passwort..." />
+            <input type="passwort" className="loginInput" placeholder="Enter your Passwort..." />
             <button className="loginButton">Log In</button>
             <button className="loginRegisterButton">Register</button>
         </from>
