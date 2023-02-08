0cfc1171b56e312995d1b0cde23477b0f3a0615f
fatima-masmas <87-f@gmx.de>
2023-02-08 15:47:45 +0100

    Register datein erstellen

--- ui-blog/src/pages/register/Register.jsx
+++ ui-blog/src/pages/register/Register.jsx
@@ -3,6 +3,15 @@ import "./register.css"
 export default function Register() {
   return (
     <div className="register">
+         <span className="registerTitle">Register</span>
+        <from className="registerForm">
+            <label >E-mail</label>
+            <input type="text" className="registerInput" placeholder="Enter your Email..." />
+            <label >Passwort</label>
+            <input type="passwort" className="registerInput" placeholder="Enter your Passwort..." />
+            <button className="registerButton">Register</button>
+            <button className="registerLoginButton">Log In</button>
+        </from>
       
     </div>
   )
