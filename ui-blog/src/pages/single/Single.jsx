36651c43e61d9c230784f9e147c1fc75b07b44bf
fatima-masmas <87-f@gmx.de>
2023-02-08 09:15:46 +0100

    didebar bei singel defenieren

--- ui-blog/src/pages/single/Single.jsx
+++ ui-blog/src/pages/single/Single.jsx
@@ -1,9 +1,11 @@
+import Sidebar from "../../components/sidebar/Sidebar";
 import "./single.css";
 
 export default function Single() {
   return (
     <div className="single">
-      single
+      {/* post */}
+      <Sidebar />
     </div>
   )
 }
